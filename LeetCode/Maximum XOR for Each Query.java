b0e8df94a947806d99767721f998591007d457d2
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-08 22:45:38 +0530

    08/11/2024

---
+++ LeetCode/Maximum XOR for Each Query.java
@@ -0,0 +1,24 @@
+/*You are given a sorted array nums of n non-negative integers and an integer maximumBit. You want to perform the following query n times:
+
+Find a non-negative integer k < 2maximumBit such that nums[0] XOR nums[1] XOR ... XOR nums[nums.length-1] XOR k is maximized. k is the answer to the ith query.
+Remove the last element from the current array nums.
+Return an array answer, where answer[i] is the answer to the ith query.*/
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int n = nums.length;
+        int xorr = nums[0];
+        int maxXor = (1 << maximumBit) - 1;
+
+        for (int i = 1; i < n; i++) {
+            xorr ^= nums[i];
+        }
+
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = xorr ^ maxXor;
+            xorr ^= nums[n - 1 - i];
+        }
+
+        return ans;
+    }
+}
