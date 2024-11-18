83277ae270d9118533cee94b86e5304f2bf8742f
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-18 07:45:07 +0530

    Diffuse the Bomb.java

---
+++ LeetCode/Diffuse the Bomb.java
@@ -0,0 +1,38 @@
+/*You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+
+To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous k numbers.
+If k == 0, replace the ith number with 0.
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!*/
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] arr = new int[n];
+        if (k > 0) {
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
+                }
+                arr[i] = sum;
+            }
+        } else if (k < 0) {
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= -k; j++) {
+                    sum += code[(i - j + n) % n];
+                }
+                arr[i] = sum;
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                arr[i] = 0;
+            }
+        }
+        return arr;
+    }
+}
