68aaded43f097f6c2d366caaad69549d83c50965
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-27 08:11:15 +0530

    03/11/2024

--- LeetCode/Rotate String.java
+++ LeetCode/Rotate String.java
@@ -1,6 +1,7 @@
 /*Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
 A shift on s consists of moving the leftmost character of s to the rightmost position.
-For example, if s = "abcde", then it will be "bcdea" after one shift.*/
+For example, if s = "abcde", then it will be "bcdea" after one shift.*/ 
+
 class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length())return false;
