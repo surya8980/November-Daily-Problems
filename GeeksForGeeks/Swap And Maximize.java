dd64d1d3e777eb8ccdd729b1d768bd94abc8d0f2
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-27 08:42:01 +0530

    01/11/2024

--- GeeksForGeeks/Swap And Maximize.java
+++ GeeksForGeeks/Swap And Maximize.java
@@ -1,5 +1,6 @@
-/*Given an array arr[ ] of positive elements. Consider the array as a circular array, meaning the element after the last element is the first element of the array. The task is to find the maximum sum of the absolute differences between consecutive elements with shuffling of array elements allowed i.e. shuffle the array elements and make [a1..an] such order that  |a1 – a2| + |a2 – a3| + …… + |an-1 – an| + |an – a1| is maximized.
-
+/*Given an array arr[ ] of positive elements. Consider the array as a circular array, meaning the element after the last element is the first element of the array.
+The task is to find the maximum sum of the absolute differences between consecutive elements with shuffling of array elements allowed i.e.
+shuffle the array elements and make [a1..an] such order that  |a1 – a2| + |a2 – a3| + …… + |an-1 – an| + |an – a1| is maximized.
 Examples:
 
 Input: arr[] = [4, 2, 1, 8]
