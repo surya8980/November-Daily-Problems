ef97b546163c32f72b3a96cc2720cbb3e571fbf7
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-11-27 08:44:55 +0530

    04/11/2024

--- GeeksForGeeks/Find all triplets with zero sum.java
+++ GeeksForGeeks/Find all triplets with zero sum.java
@@ -1,5 +1,5 @@
 /*Given an array arr[], find all possible indices [i, j, k] of triplets [arr[i], arr[j], arr[k]] in the array whose sum is equal to zero.
-Return indices of triplets in any order and all the returned triplets indices should also be internally sorted, i.e., 
+Return indices of triplets in any order and all the returned triplets indices should also be internally sorted, i.e.
 for any triplet indices [i, j, k], the condition i < j < k should hold.
 */
 class Solution {
